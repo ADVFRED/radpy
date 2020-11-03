938caf9b3773b6585f0a48ac830cb785fe027ac7
Liam  Crowley <theadvfred@gmail.com>
2020-11-03 15:41:47 +0000

    Initial setup and templating for primeGenerator

---
+++ primegen.py
@@ -0,0 +1,7 @@
+def PrimeGenerator(NumPrimesToGen,PrimeSeed=[2],NumLoopIters=None):
+  if NumPrimesToGen >= 1 and NumLoopIters==None:
+    #1st Config: Generate to a number of prime numbers
+    pass
+  if NumLoopIters != None and type(NumLoopIters)==int and NumPrimesToGen==None:
+    #2nd Config: Generate prime numbers to a set number of loop iterations.
+    pass
