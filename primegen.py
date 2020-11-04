a09662d690dfcbba80534ade18c51b009364c3ff
Liam  Crowley <theadvfred@gmail.com>
2020-11-04 17:41:25 +0000

    Shifted code to focus on one feature,others will be implemented post-releasel

--- primegen.py
+++ primegen.py
@@ -1,7 +1,4 @@
-def PrimeGenerator(NumPrimesToGen,PrimeSeed=[2],NumLoopIters=None):
+def PrimeGenerator(NumPrimesToGen,PrimeSeed=[2]):
   if NumPrimesToGen >= 1 and NumLoopIters==None:
     #1st Config: Generate to a number of prime numbers
-    pass
-  if NumLoopIters != None and type(NumLoopIters)==int and NumPrimesToGen==None:
-    #2nd Config: Generate prime numbers to a set number of loop iterations.
     pass
